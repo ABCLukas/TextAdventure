2a4317f0939c8983224168b36369692153cb5457
Lukas <schneiderluk548@gmail.com>
2024-05-31 11:48:13 +0200

    Leerzeichen entfernen

--- mavenproject1/src/main/java/textadventure/test.java
+++ mavenproject1/src/main/java/textadventure/test.java
@@ -10,8 +10,6 @@ package textadventure;
  */
 public class test {
     public static void main(String[] args) {
-        
-    
-    System.out.println("Hallo");
+        System.out.println("Hallo");
     }
 }
