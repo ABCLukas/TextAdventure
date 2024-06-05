359d32de995d2dec0ff59253b7590e49e7a2735d
DerUltraUltnerKompi <jonas.kompatscher@gmail.com>
2024-06-05 13:00:42 +0200

    hallo

--- mavenproject1/src/main/java/textadventure/Frage.java
+++ mavenproject1/src/main/java/textadventure/Frage.java
@@ -3,39 +3,30 @@ package textadventure;
 
 public class Frage {
 
-	private String frage;
+	private String titel;
+	private String text;
 	private String antwort1;
 	private String antwort2;
-	private String antwort3;
-	private String antwort4;
 	private int richtigeAntwort;
 	
-	public Frage(String frage, String antwort1, String antwort2, String antwort3, String antwort4,
-			int richtigeAntwort) {
-		this.frage = frage;
+	public Frage(String titel, String text, String antwort1, String antwort2, String word4, int richtigeAntwort) {
+		this.titel = titel;
+		this.text = text;
 		this.antwort1 = antwort1;
 		this.antwort2 = antwort2;
-		this.antwort3 = antwort3;
-		this.antwort4 = antwort4;
 		this.richtigeAntwort = richtigeAntwort;
                 
 	}
 	
 	
 	public String getFrage() {
-		return frage;
+		return titel;
 	}
 	public String getAntwort1() {
 		return antwort1;
 	}
 	public String getAntwort2() {
 		return antwort2;
-	}
-	public String getAntwort3() {
-		return antwort3;
-	}
-	public String getAntwort4() {
-		return antwort4;
 	}
 	public int getRichtigeAntwort() {
             
