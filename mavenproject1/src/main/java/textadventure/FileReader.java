af94e4dbf50755458beb284d98ac798da80464bb
matti0518 <mathias.schwienbacher2809@berufsschule.bz>
2024-06-12 17:52:06 +0200

    .

--- mavenproject1/src/main/java/textadventure/FileReader.java
+++ mavenproject1/src/main/java/textadventure/FileReader.java
@@ -5,80 +5,80 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class FileReader {
-public static int counter=0;
-    
+
+    public static int counter = 0;
 
     public static String reader(int nr) {
-      String data = "";
-      String dateiname = "";
-    switch (nr) {
-        case 1:
-            dateiname = "englishtext/crowbar.txt";
-            break;
-        case 2:
-            dateiname = "englishtext/destroyed_window_work_room.txt";//schaug mol ob richtig gschrieben isch(isch net fixt wer ondores) isch richtig super Gemacht!!!
-            break;
-        case 3:
-            dateiname = "englishtext/dropped_weapon.txt";
-            break;
-        case 4:
-            dateiname = "englishtext/entered_medical_room.txt";
-            break;
+        String data = "";
+        String dateiname = "";
+        switch (nr) {
+            case 1:
+                dateiname = "englishtext/crowbar.txt";
+                break;
+            case 2:
+                dateiname = "englishtext/destroyed_window_work_room.txt";//schaug mol ob richtig gschrieben isch(isch net fixt wer ondores) isch richtig super Gemacht!!!
+                break;
+            case 3:
+                dateiname = "englishtext/dropped_weapon.txt";
+                break;
+            case 4:
+                dateiname = "englishtext/entered_medical_room.txt";
+                break;
             case 5:
-            dateiname = "englishtext/first_scene.txt";
-            break;
+                dateiname = "englishtext/first_scene.txt";
+                break;
             case 6:
-            dateiname = "englishtext/hope_enjoyed.txt";
-            break;
+                dateiname = "englishtext/hope_enjoyed.txt";
+                break;
             case 7:
-            dateiname = "englishtext/left_door.txt";
-            break;
+                dateiname = "englishtext/left_door.txt";
+                break;
             case 8:
-            dateiname = "englishtext/opened_window.txt";
-            break;
+                dateiname = "englishtext/opened_window.txt";
+                break;
             case 9:
-            dateiname = "englishtext/rescue_manuel.txt";
-            break;
+                dateiname = "englishtext/rescue_manuel.txt";
+                break;
             case 10:
-            dateiname = "englishtext/right_door.txt";
-            break;
+                dateiname = "englishtext/right_door.txt";
+                break;
             case 11:
-            dateiname = "englishtext/running_out_medical_room.txt";
-            break;
+                dateiname = "englishtext/running_out_medical_room.txt";
+                break;
             case 12:
-            dateiname = "englishtext/stairs_down.txt";
-            break;
+                dateiname = "englishtext/stairs_down.txt";
+                break;
             case 13:
-            dateiname = "englishtext/staris_up.txt";
-            break;
+                dateiname = "englishtext/staris_up.txt";
+                break;
             case 14:
-            dateiname = "englishtext/stay_living_room.txt";
-            break;
+                dateiname = "englishtext/stay_living_room.txt";
+                break;
             case 15:
-            dateiname = "englishtext/threw_hammer.txt";
-            break;
+                dateiname = "englishtext/threw_hammer.txt";
+                break;
             case 16:
-            dateiname = "englishtext/work_room.txt";
-            break;
+                dateiname = "englishtext/work_room.txt";
+                break;
             default:
-            break;
+                break;
+        }
+        try {
+            File myObj = new File(dateiname);
+            Scanner myReader = new Scanner(myObj);
+            while (myReader.hasNextLine()) {
+                data += myReader.nextLine();
+                data += "\n";
+            }
+
+            myReader.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+        return data;
     }
-    try {
-      File myObj = new File(dateiname);
-      Scanner myReader = new Scanner(myObj);
-      while (myReader.hasNextLine()) {
-         data += myReader.nextLine();
-        data += "\n";
-      }
-        
-        
-      myReader.close();
-    } catch (FileNotFoundException e) {
-      System.out.println("An error occurred.");
-      e.printStackTrace();
-    }
-    return data;
-  }
+
     //lei zum debuggen
     public static void main(String[] args) {
         System.out.println(reader(2));
